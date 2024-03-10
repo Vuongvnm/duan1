f583086b26d883783ca4f634303522d1ab4f3963
vuongvu <vuongvu10899@gmail.com>
2024-03-10 20:54:56 +0700

    dev_vuong

---
+++ admin/controller/danhmuc/danhsach_danhmuc.php
@@ -0,0 +1,6 @@
+<?php
+  include "model/danh_muc.php";
+
+  $danhsach_danhmuc = hienthi_danhmuc();
+  include "view/danh_muc/danhsach_danhmuc.php";
+?>
---
+++ view/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ecommerce Website</title>
+    <link rel="stylesheet" href="../css/style.css" />
+    <script
+      src="https://kit.fontawesome.com/47e370afdb.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <!--header-->
+    <section id="header">
+      <a href=""><img src="../images/logo1.png" class="logo" alt=""></a>
+
+      <ul id="navbar">
+        <li><a class="active" href="index.html">Home</a></li>
+        <li><a href="shop.html">Shop</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li id="lg-bar"><a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a></li>
+        <a href="#" id="close"><i class="fa-solid fa-x"></i></a>
+      </ul>
+      <div id="mobile">
+        <a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a>
+        <i id="bar" class="fa-solid fa-outdent"></i>
+      </div>
+    </section>
+
+    <!--hero-->
+    <section id="hero">
+      <h4>Trade-in-offer</h4>
+      <h2>Super value deals</h2>
+      <h1>On all products</h1>
+      <p>Save more with coupons & up to 70% off!</p>
+      <button>Shop Now</button>
+    </section>
+
+    <!--feature-->
+    <section id="feature" class="section-p1">
+      <div class="fe-box">
+        <img src="../images/features/f1.png" alt="" />
+        <h6>Free Shipping</h6>
+      </div>
+      <div class="fe-box">
+        <img src="../images/features/f2.png" alt="" />
+        <h6>Online Order</h6>
+      </div>
+      <div class="fe-box">
+        <img src="../images/features/f3.png" alt="" />
+        <h6>Save Money</h6>
+      </div>
+      <div class="fe-box">
+        <img src="../images/features/f4.png" alt="" />
+        <h6>Promotions</h6>
+      </div>
+      <div class="fe-box">
+        <img src="../images/features/f5.png" alt="" />
+        <h6>Happy Sell</h6>
+      </div>
+      <div class="fe-box">
+        <img src="../images/features/f6.png" alt="" />
+        <h6>F24/7 Support</h6>
+      </div>
+    </section>
